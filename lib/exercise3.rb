95470863074da346d828d5d2b3176a939223b668
Jessica Bealey <jfbealey@gmail.com>
2023-05-12 16:30:15 +0100

    Removed whitespace and fixed sorting

--- lib/exercise3.rb
+++ lib/exercise3.rb
@@ -1,14 +1,14 @@
 def get_most_common_letter(text)
     counter = Hash.new(0)
     #p counter
-    text.chars.each do |char|
-        #p "#{char}"
+    text.gsub(/\s+/, "").chars.each do |char|
+   # p "#{char}"
       counter[char] += 1
-      #p counter
+   #p counter
     end
-     #p counter.to_a
-     p counter.to_a.sort_by { |k, v| v }
-     #p counter
+    #p counter.to_a
+     counter.to_a.sort_by { |k, v| -v }[0][0]
+    #p counter
   end
 
-print get_most_common_letter("Hello there")
+print get_most_common_letter("Hel   lo!!! ")
