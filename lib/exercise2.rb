efee7b941440c746fc6427ac0155122128fad632
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-12 14:39:01 +0100

    Pushing files and debugging challenge

--- lib/exercise2.rb
+++ lib/exercise2.rb
@@ -1,9 +1,11 @@
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  puts "This is what the cipher does #{cipher}"
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char)).chr
   end
   return ciphertext_chars.join
+  print "This is what the ciphertext_chars does #{ciphertext_chars}"
 end
 
 def decode(ciphertext, key)
@@ -16,8 +18,9 @@ end
 
 # Intended output:
 #
-# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
-# => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
 #
-# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
+
